0e8530b5b42292e3e40f3745568d3f7a682cdd4a
vsevolodgrigorev <sevagrigoryev@gmail.com>
2024-03-07 11:27:20 +0300

    ignore

--- src/main/resources/public.daily_picks.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE public.daily_picks (
-    id SERIAL PRIMARY KEY,
-    chat_id VARCHAR(255) NOT NULL,
-    user_id INTEGER NOT NULL,
-    pick_date DATE NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id)
-);
